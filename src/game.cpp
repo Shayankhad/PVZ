dc1f185bdb167dc698ff7a36a74e90a4636d8b94
Shayankhad <shayan.khademi7812@gmail.com>
2024-05-10 07:59:13 -0400

    add start pos arg in constructor

--- src/game.cpp
+++ src/game.cpp
@@ -2,25 +2,22 @@
 
 class Zombie{
 public:
-    Zombie(){
+    Zombie(Vector2f start_pos){
         if(!zombie_texture.loadFromFile(ZOMBIE_IMAGE_ADDRESS)){
             cerr << "failed to load zombie image";
         }
         zombie_sprite.setTexture(zombie_texture);
+        //zombie_sprite.setScale(50 , 50);
         zombie_sprite.setPosition(start_pos);
     }
     void render_zombie(RenderWindow &window){
         window.draw(zombie_sprite);
     }
-    void set_start_pos(Vector2f start_position){
-        start_pos = start_position;
-    }
+
 private:
     Texture zombie_texture;
     Sprite zombie_sprite;
-    Vector2f start_pos;
 };
-
 
 class Game{
 public:
@@ -31,8 +28,9 @@ public:
             cerr << "fialed to open frontyard image!!!";
         }
         frontyard_sprite.setTexture(frontyard_tex);
-        zombie = new Zombie();
-        zombie->set_start_pos({1300 , 300});
+        zombie = new Zombie({600 , 300});
+        
+        
     }
     void mouse_press_handeling(){
         while(window.pollEvent(event)){
@@ -59,7 +57,6 @@ private:
     Texture frontyard_tex;
     Sprite frontyard_sprite;
 };
-
 
 
 int main(){
