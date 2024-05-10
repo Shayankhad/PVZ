fdcbaf1e51111e63974252cae74c53cff3b4014d
Shayankhad <shayan.khademi7812@gmail.com>
2024-05-10 02:34:29 -0400

    circle_shape

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,37 @@
 #include "global.hpp"
 
 class Game{
-    
+public:
+    Game(){
+        window.create(VideoMode(X_WINDOW , Y_WINDOW) , WINDOWS_TITLE);
+        circle_shape.setRadius(50);
+        circle_shape.setFillColor(Color::Magenta);
+        circle_shape.setOutlineColor(Color::Green);
+        circle_shape.setOutlineThickness(3);
+    }
+    void run(){
+        while(window.isOpen()){
+            Event event;
+            while(window.pollEvent(event)){
+                if(event.type == Event::EventType::Closed){
+                    window.close();  
+                }   
+            }
+            window.clear(Color::Black);
+            window.draw(circle_shape);
+            window.display();
+
+        }
+    }
+private:
+    RenderWindow window;
+    CircleShape circle_shape;
+
 };
+
+int main(){
+    Game pvz;
+    pvz.run();
+    CircleShape circle_shape(50);
+    return 0;
+}
