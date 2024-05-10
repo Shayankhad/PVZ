dcd8c27024d50bd435a26133e2c38b958cac3052
Shayankhad <shayan.khademi7812@gmail.com>
2024-05-10 03:21:38 -0400

    made a mous_press_handeling method

--- src/game.cpp
+++ src/game.cpp
@@ -8,30 +8,37 @@ public:
         circle_shape.setFillColor(Color::Magenta);
         circle_shape.setOutlineColor(Color::Green);
         circle_shape.setOutlineThickness(3);
+        rect_shape = RectangleShape(Vector2f( 50, 50));
+        rect_shape.setFillColor(Color::Red);
+        rect_shape.setPosition(100 , 100);
+        
+    }
+    void mouse_press_handeling(){
+        while(window.pollEvent(event)){
+            if(event.type == Event::EventType::Closed){
+                window.close();
+            }
+        }
     }
     void run(){
         while(window.isOpen()){
-            Event event;
-            while(window.pollEvent(event)){
-                if(event.type == Event::EventType::Closed){
-                    window.close();  
-                }   
-            }
             window.clear(Color::Black);
+            mouse_press_handeling();
             window.draw(circle_shape);
+            window.draw (rect_shape);
             window.display();
-
         }
     }
 private:
     RenderWindow window;
     CircleShape circle_shape;
+    RectangleShape rect_shape;
+    Event event;
 
 };
 
 int main(){
     Game pvz;
     pvz.run();
-    CircleShape circle_shape(50);
     return 0;
 }
