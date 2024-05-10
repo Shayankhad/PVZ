bd588120b064426a3f6ae3e326fd6f165cfa1e68
Shayankhad <shayan.khademi7812@gmail.com>
2024-05-10 06:19:56 -0400

    add frontyard without using sprite

--- src/game.cpp
+++ src/game.cpp
@@ -4,6 +4,14 @@ class Game{
 public:
     Game(){
         window.create(VideoMode(X_WINDOW , Y_WINDOW) , WINDOWS_TITLE);
+        if(!frontyard_tex.loadFromFile(BACKGROUND_IMAGE_ADDRESS)){
+            cerr << "fialed to open frontyard image!!!";
+        }
+        Vector2f screen_size = {X_WINDOW , Y_WINDOW};
+        rec_shape.setSize(screen_size);
+        rec_shape.setTexture(&frontyard_tex);
+
+
     }
     void mouse_press_handeling(){
         while(window.pollEvent(event)){
@@ -16,13 +24,15 @@ public:
         while(window.isOpen()){
             window.clear(Color::Black);
             mouse_press_handeling();
+            window.draw(rec_shape);
             window.display();
         }
     }
 private:
     RenderWindow window;
     Event event;
-
+    Texture frontyard_tex;
+    RectangleShape rec_shape;
 };
 
 int main(){
