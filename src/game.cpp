f2c3fd8ebab8becc19c7972ba13681d5538894c1
Shayankhad <shayan.khademi7812@gmail.com>
2024-05-10 03:36:29 -0400

    prepearing the game class

--- src/game.cpp
+++ src/game.cpp
@@ -4,14 +4,6 @@ class Game{
 public:
     Game(){
         window.create(VideoMode(X_WINDOW , Y_WINDOW) , WINDOWS_TITLE);
-        circle_shape.setRadius(50);
-        circle_shape.setFillColor(Color::Magenta);
-        circle_shape.setOutlineColor(Color::Green);
-        circle_shape.setOutlineThickness(3);
-        rect_shape = RectangleShape(Vector2f( 50, 50));
-        rect_shape.setFillColor(Color::Red);
-        rect_shape.setPosition(100 , 100);
-        
     }
     void mouse_press_handeling(){
         while(window.pollEvent(event)){
@@ -24,15 +16,11 @@ public:
         while(window.isOpen()){
             window.clear(Color::Black);
             mouse_press_handeling();
-            window.draw(circle_shape);
-            window.draw (rect_shape);
             window.display();
         }
     }
 private:
     RenderWindow window;
-    CircleShape circle_shape;
-    RectangleShape rect_shape;
     Event event;
 
 };
