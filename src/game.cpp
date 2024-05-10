01d9ba17eb5f5560f4e856892de0b51b1cb70fd2
Shayankhad <shayan.khademi7812@gmail.com>
2024-05-10 08:29:41 -0400

    made zombie move and deleted setbounds for zombies

--- src/game.cpp
+++ src/game.cpp
@@ -3,16 +3,19 @@
 class Zombie{
 public:
     Zombie(Vector2f start_pos){
+
         if(!zombie_texture.loadFromFile(ZOMBIE_IMAGE_ADDRESS)){
             cerr << "failed to load zombie image";
         }
+        
         zombie_sprite.setTexture(zombie_texture);
         zombie_sprite.setScale({0.2 , 0.2});
         zombie_sprite.setPosition(start_pos);
-
     }
     void render_zombie(RenderWindow &window){
+
         window.draw(zombie_sprite);
+        zombie_sprite.move(-0.2 , 0);
     }
 
 private:
@@ -29,9 +32,7 @@ public:
             cerr << "fialed to open frontyard image!!!";
         }
         frontyard_sprite.setTexture(frontyard_tex);
-        zombie = new Zombie({0 , 0});
-        
-        
+        zombie = new Zombie({900 , 50});
     }
     void mouse_press_handeling(){
         while(window.pollEvent(event)){
