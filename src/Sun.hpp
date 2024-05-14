f148fbc7ec02d360337f1fba363459f8c0d2df3e
AlirezaALA <alirezaabdi209@gmail.com>
2024-05-14 12:09:45 +0330

    setup the movment of the sun

--- src/Sun.hpp
+++ src/Sun.hpp
@@ -15,6 +15,7 @@ public:
         sun_sprite.setPosition(300 ,300);
     }
     void render_sun(RenderWindow *window){
+        sun_sprite.move(0 , 0.3);
         window->draw(sun_sprite);
     }
     Sprite* get_sun_sprite(){
