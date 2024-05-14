a820f7bb1f9e16bbe19bd08075b23e96b4b72cea
AlirezaALA <alirezaabdi209@gmail.com>
2024-05-14 23:08:19 +0330

    CREATE SUNFLOWER CLASS

---
+++ sunflower_label.hpp
@@ -0,0 +1,31 @@
+#ifndef SUNFLOWER_LABEL_HPP
+#define SUNFLOWER_LABEL_HPP
+
+#include "global.hpp"
+
+class Sunflower_label {
+public:
+    Sunflower_label() {
+        if (!sunflower_label_texture.loadFromFile(SUNFLOWER_LABEL_ADDRESS)) {
+            cerr << "Failed to load sunflower label image";
+        }
+        sunflower_label_sprite.setTexture(sunflower_label_texture);
+        sunflower_label_sprite.setPosition({0, 0});
+        sunflower_label_sprite.setScale(0.5, 0.5);
+    }
+
+    void render_sunflower_label(RenderWindow& window) {
+        window.draw(sunflower_label_sprite);
+    }
+
+    Sprite* get_sunflower_label() {
+        return &sunflower_label_sprite;
+    }
+
+private:
+    Texture sunflower_label_texture;
+    Sprite sunflower_label_sprite;
+};
+
+#endif
+
